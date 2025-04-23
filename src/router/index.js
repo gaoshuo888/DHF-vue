17ddd6ada753aeaac8593c3835aef15493478796
Shuo Gao <gshuo.666@gmail.com>
2025-04-23 16:31:53 +0800

    创建book，dhf，qchart目录，将view组件分类

--- src/router/index.js
+++ src/router/index.js
@@ -7,38 +7,38 @@ const routes = [
     {
         path: '/',//指定首页组件——默认首先看到的组件
         name: '图书管理',
-        component: () => import(/* webpackChunkName: "home" */ '../views/Navigation.vue'),
+        component: () => import(/* webpackChunkName: "home" */ '../components/Navigation.vue'),
         show: true,//是否显示在侧边栏
         children: [//指定子路由
             {
                 path: '/bookSearch',
                 name: '查询',
-                component: () => import(/* webpackChunkName: "PageOne" */ '../views/BookSearch.vue')
+                component: () => import(/* webpackChunkName: "PageOne" */ '../views/book/BookSearch.vue')
             }
         ]
     },
     {
         path: '/navigation1',
         name: '数据图表',
-        component: () => import(/* webpackChunkName: "navigation1" */ '../views/Navigation.vue'),
+        component: () => import(/* webpackChunkName: "navigation1" */ '../components/Navigation.vue'),
         show: true,//是否显示在侧边栏
         children: [
             {
                 path: '/qChart',
                 name: '折线图',
-                component: () => import(/* webpackChunkName: "PageOne" */ '../views/Q_chart.vue'),
+                component: () => import(/* webpackChunkName: "PageOne" */ '../views/qchart/Q_chart.vue'),
             },
             {
                 path: '/qData',
                 name: '数据表',
-                component: () => import(/* webpackChunkName: "PageOne" */ '../views/QSearch.vue')
+                component: () => import(/* webpackChunkName: "PageOne" */ '../views/qchart/QSearch.vue')
             }
         ]
     },
     {
         path: '/navigation2',
         name: '前后端交互',
-        component: () => import(/* webpackChunkName: "navigation2" */ '../views/Navigation.vue'),
+        component: () => import(/* webpackChunkName: "navigation2" */ '../components/Navigation.vue'),
         show: true,//是否显示在侧边栏
         children: [
             {
@@ -49,30 +49,44 @@ const routes = [
         ]
     },
     {
+        path: '/center',
+        name: 'DHF模型',
+        show: true,
+        component: () => import(/* webpackChunkName: "PageOne" */ '../components/Navigation.vue'),
+        children: [
+            {
+                path: '/dhfMap',
+                name: '英那河流域图',
+                component: () => import(/* webpackChunkName: "PageOne" */ '../views/dhf/WatershedMap.vue')
+            }
+
+        ]
+    },
+    {
         path: '/navigation3',
         name: '不展示导航栏',
         show: false,//是否显示在侧边栏
-        component: () => import(/* webpackChunkName: "PageFour" */ '../views/Navigation.vue'),
+        component: () => import(/* webpackChunkName: "PageFour" */ '../components/Navigation.vue'),
         children: [
             {
                 path: '/addBook',
                 name: '添加',
-                component: () => import(/* webpackChunkName: "PageTwo" */ '../views/BookAdd.vue')
+                component: () => import(/* webpackChunkName: "PageTwo" */ '../views/book/BookAdd.vue')
             },
             {
                 path: '/bookUpdate',
                 name: '修改',
-                component: () => import(/* webpackChunkName: "PageFour" */ '../views/BookUpdate.vue')
+                component: () => import(/* webpackChunkName: "PageFour" */ '../views/book/BookUpdate.vue')
             },
             {
                 path: '/addQ',
                 name: '添加数据',
-                component: () => import(/* webpackChunkName: "PageFour" */ '../views/QAdd.vue')
+                component: () => import(/* webpackChunkName: "PageFour" */ '../views/qchart/QAdd.vue')
             },
             {
                 path: '/qUpdate',
                 name: '数据表修改',
-                component: () => import(/* webpackChunkName: "PageFour" */ '../views/QUpdate.vue')
+                component: () => import(/* webpackChunkName: "PageFour" */ '../views/qchart/QUpdate.vue')
             }
         ]
     }
---
+++ src/views/dhf/Center.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="center">
+        <h1>Welcome to the Center Page</h1>
+        <p>This is the center page of the application.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Center"
+}
+</script>
+
+<style scoped>
+
+</style>
