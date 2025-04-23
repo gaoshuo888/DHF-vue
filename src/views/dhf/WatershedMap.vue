0f71e09d924d6aeecd739e30b9eb6434a3a24f29
Shuo Gao <gshuo.666@gmail.com>
2025-04-23 16:40:24 +0800

    添加图片样式

--- src/views/dhf/WatershedMap.vue
+++ src/views/dhf/WatershedMap.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="英那河流域图" src="../../assets/WatershedMap.jpg">
-  </div>
+    <div class="home">
+        <img alt="英那河流域图" src="../../assets/WatershedMap.jpg" class="watershed-image">
+    </div>
 </template>
 
 <script>
-
+// 如果需要，可以在这里添加脚本逻辑
 </script>
+
+<style scoped>
+.watershed-image {
+    width: 60%;
+    height: auto;
+    max-width: 1000px; /* 设置最大宽度，防止图片被拉伸 */
+    display: block;
+    margin: 0 auto;
+}
+</style>
